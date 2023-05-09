a1c9a2778b79418f7149a39f5bfdef9824282c6d
Nathan <nathanrambalducci@edu.unifil.br>
2023-05-09 16:04:13 -0300

    Commit de hj

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,43 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::thread::Builder;
 #[tokio::main]
 
-use hyper::{Server};
+use hyper::{service::{make_service_fn, service_fn}, Request, Body, Responce};
+use hyper::header::SERVER;
+use hyper::Response;
+use hyper::server::conn::AddrIncoming;
+use tokio::net::windows::named_pipe::PipeEnd::Client;
+use tower::MakeService;
+use tower::make::Shared;
 
-fn main() {
+async fn main() {
+    let make_service = Shared::new(service_fn(handle()));
+    let addr = SocketAddr::from([127, 0, 0, 1], 3000);
+    let server = Server::bind(&addr).serve(make_service);
 
+    if let Err(e) = server.await {
+        println!("error: {}", e)
+    }
+}
+
+async fn handle(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+
+    //Ok(Response::new(Body::from("Hello from HTTP proxy")))
+    let client = Client::new();
+    client.request(req).await
+
+}
+
+async fn log(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+
+    let path = req.uri().path();
+
+    if path.starts_with("/api") {
+        println!("API Path = {}", path);
+    } else {
+        println!("Generic path = {}", path)
+    }
+
+    handler(req).await
 }
