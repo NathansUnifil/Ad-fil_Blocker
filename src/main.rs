7ef6a460f9dde21625e42a87623867a7c7a04d74
Nathan <nathanrambalducci@edu.unifil.br>
2023-05-16 17:37:22 -0300

    Ok novo plano, novas ideias
    Usar o request do hyper para procurar todas as horas com await os links e bloquear
    
    Porque o reqwest não aceita futures e sem eles, o programa acaba na hora
    
    Talvez usar um while, mais vou deixar por ultimo
    
    Tentou ver se trabalho mais depois da aula

--- src/main.rs
+++ src/main.rs
@@ -16,53 +16,189 @@ async fn main() {
 	"*://*.scorecardresearch.com/*",
 	"*://*.zedo.com/*",];
 
-	let resp1 = match reqwest::blocking::get("*://*.doubleclick.net/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let mut client = hyper::Client::new();
+
+	// Pass our request builder object to our client.
+	let hyperresp1 = client.request(req1).await?;
+
+	let req1 = hyper::Request::builder()
+		.method(hyper::Method::GET)
+		.uri("*://*.doubleclick.net/*")
+		.header("user-agent", "the-awesome-agent/007")
+		.body(hyper::Body::from(""))?;
+
+	// Get the response body bytes.
+	let body_bytes1 = hyper::body::to_bytes(hyperresp1.into_body()).await?;
+
+	// Convert the body bytes to utf-8
+	let body1 = String::from_utf8(body_bytes1.to_vec()).unwrap();
+
+	// 2
+
+	// Pass our request builder object to our client.
+	let hyperresp1 = client.request(req1).await?;
+
+	let req1 = hyper::Request::builder()
+		.method(hyper::Method::GET)
+		.uri("*://*.doubleclick.net/*")
+		.header("user-agent", "the-awesome-agent/007")
+		.body(hyper::Body::from(""))?;
+
+	// Get the response body bytes.
+	let body_bytes1 = hyper::body::to_bytes(hyperresp1.into_body()).await?;
+
+	// Convert the body bytes to utf-8
+	let body1 = String::from_utf8(body_bytes1.to_vec()).unwrap();
+
+	// 2
+
+	// Pass our request builder object to our client.
+	let hyperresp1 = client.request(req1).await?;
+
+	let req1 = hyper::Request::builder()
+		.method(hyper::Method::GET)
+		.uri("*://*.doubleclick.net/*")
+		.header("user-agent", "the-awesome-agent/007")
+		.body(hyper::Body::from(""))?;
+
+	// Get the response body bytes.
+	let body_bytes1 = hyper::body::to_bytes(hyperresp1.into_body()).await?;
+
+	// Convert the body bytes to utf-8
+	let body1 = String::from_utf8(body_bytes1.to_vec()).unwrap();
+
+	// 2
+
+	// Pass our request builder object to our client.
+	let hyperresp1 = client.request(req1).await?;
+
+	let req1 = hyper::Request::builder()
+		.method(hyper::Method::GET)
+		.uri("*://*.doubleclick.net/*")
+		.header("user-agent", "the-awesome-agent/007")
+		.body(hyper::Body::from(""))?;
+
+	// Get the response body bytes.
+	let body_bytes1 = hyper::body::to_bytes(hyperresp1.into_body()).await?;
+
+	// Convert the body bytes to utf-8
+	let body1 = String::from_utf8(body_bytes1.to_vec()).unwrap();
+
+	// 2
+
+	// Pass our request builder object to our client.
+	let hyperresp1 = client.request(req1).await?;
+
+	let req1 = hyper::Request::builder()
+		.method(hyper::Method::GET)
+		.uri("*://*.doubleclick.net/*")
+		.header("user-agent", "the-awesome-agent/007")
+		.body(hyper::Body::from(""))?;
+
+	// Get the response body bytes.
+	let body_bytes1 = hyper::body::to_bytes(hyperresp1.into_body()).await?;
+
+	// Convert the body bytes to utf-8
+	let body1 = String::from_utf8(body_bytes1.to_vec()).unwrap();
+
+	// 2
+
+	// Pass our request builder object to our client.
+	let hyperresp1 = client.request(req1).await?;
+
+	let req1 = hyper::Request::builder()
+		.method(hyper::Method::GET)
+		.uri("*://*.doubleclick.net/*")
+		.header("user-agent", "the-awesome-agent/007")
+		.body(hyper::Body::from(""))?;
+
+	// Get the response body bytes.
+	let body_bytes1 = hyper::body::to_bytes(hyperresp1.into_body()).await?;
+
+	// Convert the body bytes to utf-8
+	let body1 = String::from_utf8(body_bytes1.to_vec()).unwrap();
+
+	// 2
+
+	// Pass our request builder object to our client.
+	let hyperresp1 = client.request(req1).await?;
+
+	let req1 = hyper::Request::builder()
+		.method(hyper::Method::GET)
+		.uri("*://*.doubleclick.net/*")
+		.header("user-agent", "the-awesome-agent/007")
+		.body(hyper::Body::from(""))?;
+
+	// Get the response body bytes.
+	let body_bytes1 = hyper::body::to_bytes(hyperresp1.into_body()).await?;
+
+	// Convert the body bytes to utf-8
+	let body1 = String::from_utf8(body_bytes1.to_vec()).unwrap();
+
+	// 2
+
+	// Pass our request builder object to our client.
+	let hyperresp1 = client.request(req1).await?;
+
+	let req1 = hyper::Request::builder()
+		.method(hyper::Method::GET)
+		.uri("*://*.doubleclick.net/*")
+		.header("user-agent", "the-awesome-agent/007")
+		.body(hyper::Body::from(""))?;
+
+	// Get the response body bytes.
+	let body_bytes1 = hyper::body::to_bytes(hyperresp1.into_body()).await?;
+
+	// Convert the body bytes to utf-8
+	let body1 = String::from_utf8(body_bytes1.to_vec()).unwrap();
+
+	let resp1 = match reqwest::blocking::get(body1) {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp1);
-	let resp2 = match reqwest::blocking::get("*://partner.googleadservices.com/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let resp2 = match reqwest::blocking::get("*://partner.googleadservices.com/*") {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp2);
-	let resp3 = match reqwest::blocking::get("*://*.googlesyndication.com/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let resp3 = match reqwest::blocking::get("*://*.googlesyndication.com/*") {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp3);
-	let resp4 = match reqwest::blocking::get("*://*.google-analytics.com/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let resp4 = match reqwest::blocking::get("*://*.google-analytics.com/*") {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp4);
-	let resp5 = match reqwest::blocking::get("*://creative.ak.fbcdn.net/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let resp5 = match reqwest::blocking::get("*://creative.ak.fbcdn.net/*") {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp5);
-	let resp6 = match reqwest::blocking::get("*://*.adbrite.com/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let resp6 = match reqwest::blocking::get("*://*.adbrite.com/*") {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp6);
-	let resp7 = match reqwest::blocking::get("*://*.exponential.com/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let resp7 = match reqwest::blocking::get("*://*.exponential.com/*") {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp7);
-	let resp8 = match reqwest::blocking::get("*://*.quantserve.com/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let resp8 = match reqwest::blocking::get("*://*.quantserve.com/*") {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp8);
-	let resp9 = match reqwest::blocking::get("*://*.scorecardresearch.com/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let resp9 = match reqwest::blocking::get("*://*.scorecardresearch.com/*") {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp9);
-	let resp10 = match reqwest::blocking::get("*://*.zedo.com/*").await {
-		Ok(server) => server.text().await.unwrap(),
+	let resp10 = match reqwest::blocking::get("*://*.zedo.com/*") {
+		Ok(server) => server.text().unwrap(),
 		Err(err) => panic!("Error: {}", err)
 	};
 	println!("{}", resp10);
@@ -84,25 +220,72 @@ async fn handle(req: Request<Body>) -> hyper::Result<Response<Body>> {
 
 async fn log(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
 
-	let filtros =["*://*.doubleclick.net/*",
-		"*://partner.googleadservices.com/*",
-		"*://*.googlesyndication.com/*",
-		"*://*.google-analytics.com/*",
-		"*://creative.ak.fbcdn.net/*",
-		"*://*.adbrite.com/*",
-		"*://*.exponential.com/*",
-		"*://*.quantserve.com/*",
-		"*://*.scorecardresearch.com/*",
-		"*://*.zedo.com/*",
-		"*://*.httpbin.org/*"];
-
 	let path = req.uri().path();
 
-	if path.starts_with(filtros) {
+	if path.starts_with("*://*.doubleclick.net/*") {
 		println!("Site popup encotrado encontrado = {}", path);
 	} else {
 		println!("Generic path = {}", path)
 	}
 
+	if path.starts_with("*://partner.googleadservices.com/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
+
+	if path.starts_with("*://*.googlesyndication.com/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
+
+	if path.starts_with("*://*.google-analytics.com/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
+
+	if path.starts_with("*://creative.ak.fbcdn.net/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
+
+	if path.starts_with("*://*.adbrite.com/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
+
+	if path.starts_with("*://*.exponential.com/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
+
+	if path.starts_with("*://*.quantserve.com/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
+
+	if path.starts_with("*://*.scorecardresearch.com/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
+
+	if path.starts_with("*://*.zedo.com/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
+
+	if path.starts_with("*://*.httpbin.org/*") {
+		println!("Site popup encotrado encontrado = {}", path);
+	} else {
+		println!("Generic path = {}", path)
+	}
 	handle(req).await
 }
